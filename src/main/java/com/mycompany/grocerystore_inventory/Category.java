8a7a603a913228b7ede2782069d5ecea63666d1c
ST10462466-Samuella Kitwa <ST10462466@myemeris.edu.za>
2026-09-25 10:46:27 +0200

    Implementing enum Category

---
+++ src/main/java/com/mycompany/grocerystore_inventory/Category.java
@@ -0,0 +1,11 @@
+package com.mycompany.grocerystore_inventory;
+
+// special class storing constants, in this instance categories
+public enum Category {
+    FRUITS,
+    VEGETABLES,
+    DAIRY,
+    BAKERY,
+    MEAT
+    
+}
