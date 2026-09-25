ee35c6cb6c15ec28fc9c128e644585238dabf561
ST10462466-Samuella Kitwa <ST10462466@myemeris.edu.za>
2026-09-25 10:38:38 +0200

    Implementing two classes: Item and GroceryStore_Inventory

---
+++ src/main/java/com/mycompany/grocerystore_inventory/GroceryStore_Inventory.java
@@ -0,0 +1,8 @@
+package com.mycompany.grocerystore_inventory;
+
+public class GroceryStore_Inventory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
