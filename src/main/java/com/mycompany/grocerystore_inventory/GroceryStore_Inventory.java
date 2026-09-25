d05e79b4e9e813f06e69597fb8b0d495adaccf51
ST10462466-Samuella Kitwa <ST10462466@myemeris.edu.za>
2026-09-25 19:23:37 +0200

    updating comments and adding references

--- src/main/java/com/mycompany/grocerystore_inventory/GroceryStore_Inventory.java
+++ src/main/java/com/mycompany/grocerystore_inventory/GroceryStore_Inventory.java
@@ -1,3 +1,9 @@
+// Refrences:
+//https://www.google.com/search?q=how+to+initialize+an+array+with+specific+objects+in+java%3F&sca_esv=fac01c4ba4c5f10b&sxsrf=APpeQnvz4qkGwqDL-1NdLZtI33BLk3WKQg%3A1790356368701&source=chrome.ob&fbs=ABfTbFVyMZGZf1hfvX9uKjN_-G8cqu7ocb7U6ah0xpkIrGMK4LpKPoHZrc4HVz9uOAATjT1g2XbMUNYTWk-OmYpiK--qaigyNre7a9Fsg8QLr9z5lJegIZGuhAshyGX8vZNsidOe2y3k9Yup0oEbg7vSMYlsXcLQONuEYGQeYUXcOUWQavxf8tiuiFRXsrG1FqFEc3jvZgjn&vsint=&aep=1&ntc=1&cs=1&sa=X&ved=2ahUKEwjKrIKbnYqXAxU_a0EAHQcnAOYQ2J8OegQIExAD&biw=1280&bih=559&dpr=1.5&mstk=AUtExfAoW0JKLwB_MXv1DPVcc65jrFae3K-7ZKEQRVCqUIWeOxw89EyABpucjelOfo3Osimn3P_l8WyJ2p7_vA-UsA-kuJ_1zRDJjrTZodCZxapDSUZEd771pUXKtWJq4rw5aPzT05YNG9ATJtW_JORfoyxn8D3s3XW3cMMB2Mpe7WydjiLddbTDdDbGYUBi8BW997XPEX6mlLTYYbZ3xVjJWFxrn2GQsFRO6BfGEZDzfUDy966nepAg6iW1lcE8sUKag9E6CgPbCo60cGZoUJt954lofS7IErbWkUYyu2bCmfUu1vzD_x2rW2MTF7-c8oOxCeoX0Jeyrybmmg&csuir=1&mtid=lKu2avWWFp2DhbIP0-Kk6Qs&udm=50&lns_mode=cvst
+// Above is the link of how to initialize an array with specific objects
+// https://www.w3schools.com/java/java_arraylist.asp : arraylist
+// https://www.w3schools.com/java/java_try_catch.asp : try ... catch
+
 package com.mycompany.grocerystore_inventory;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -18,10 +24,11 @@ public class GroceryStore_Inventory {
         
         };
         
-        // arraylist for the dynamic inventory
+        
+        // implementig an arraylist that will store more items than the array
         ArrayList<Item> inventory = new ArrayList<>();
         
-        // Add the array items to the arrayList
+        // adding items from the initial array to the arraylist to be more manageable
         for (Item item : initialItems) {
             inventory.add(item);
         }
@@ -61,7 +68,7 @@ public class GroceryStore_Inventory {
         input.close();
     }
         
-        // Display all items 
+        // method displaying all items stored in the arrylist
         public static void displayAllItems(ArrayList<Item> inventory) {
 
             System.out.println("\n--- ALL ITEMS ---");
@@ -72,12 +79,16 @@ public class GroceryStore_Inventory {
         
         }
         
-        // Add a new item
+        
+        // method adding a new item to the arraylist
         public static void addItem(ArrayList<Item> inventory, Scanner input) {
 
             System.out.print("Enter item name: ");
             String name = input.nextLine();
+            
 
+            // displaying the menu to allow the user to choose
+            
             System.out.println("Choose a category:");
             System.out.println("1. FRUITS");
             System.out.println("2. VEGETABLES");
@@ -114,7 +125,7 @@ public class GroceryStore_Inventory {
         }
 
         
-        // Remove an item by name
+        // method removing an item by name from the arrylist
         public static void removeItem(ArrayList<Item> inventory, Scanner input) {
 
             System.out.print("Enter the name of the item to remove: ");
@@ -137,8 +148,9 @@ public class GroceryStore_Inventory {
                 System.out.println("Item not found.");
             }
         }
+        
 
-        // Display items in a specific category
+        // method displaying only items belonging to a specific category based on the user's input
         public static void displayByCategory(ArrayList<Item> inventory, Scanner input) {
 
             System.out.print("Enter category (FRUITS, VEGETABLES, DAIRY, BAKERY, MEAT): ");
