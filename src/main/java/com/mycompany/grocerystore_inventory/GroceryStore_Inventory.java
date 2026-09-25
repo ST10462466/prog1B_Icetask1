116786467622556e0e079d3134db6804879cb5f5
ST10462466-Samuella Kitwa <ST10462466@myemeris.edu.za>
2026-09-25 11:11:30 +0200

    Initializing an array with 5 different objects

--- src/main/java/com/mycompany/grocerystore_inventory/GroceryStore_Inventory.java
+++ src/main/java/com/mycompany/grocerystore_inventory/GroceryStore_Inventory.java
@@ -1,8 +1,20 @@
 package com.mycompany.grocerystore_inventory;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GroceryStore_Inventory {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        
+        Scanner input = new Scanner(System.in);
+        
+        Item[] initialItems = {
+            new Item("Orange", Category.FRUITS, 11.95),
+            new Item("Green pepper", Category.VEGETABLES, 9.99),
+            new Item("Cheese", Category.DAIRY, 25.00),
+            new Item("Croissant", Category.BAKERY, 18.99),
+            new Item("Beef", Category.MEAT, 75.50)
+        
+        };
+   }
 }
--- src/main/java/com/mycompany/grocerystore_inventory/Item.java
+++ src/main/java/com/mycompany/grocerystore_inventory/Item.java
@@ -1,5 +1,15 @@
 package com.mycompany.grocerystore_inventory;
 
 public class Item {
+    String name;
+    Category category;
+    double price;
+    
+    
+    public Item(String name, Category category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
     
 }
